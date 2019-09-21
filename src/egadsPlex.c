5f320bad72d202dcad5679f50b16eafd454d3a13
Brandon_Denton <bldenton@buffalo.edu>
2019-09-21 15:50:23 -0400

    Code now outputs coordinates associated with a NODE, NODEs associated with an EDGEs and EDGEs associated with a LOOP

--- src/egadsPlex.c
+++ src/egadsPlex.c
@@ -8,9 +8,9 @@
 
 int main(int argc, char *argv[])
 {
-  int i, j, k, n, nn, mm, index, stat, oclass, mtype, nbodies, *senses;
+  int i, j, k, l, n, nn, mm, index, stat, oclass, mtype, nbodies, *senses;
   double limits[4];
-  ego context, model, geom, *bodies, *objs, *nobjs, *mobjs;
+  ego context, model, geom, *bodies, *objs, *nobjs, *mobjs, *lobjs;
   
   if (argc != 2) {
     printf(" Usage: liteTest liteFile\n\n");
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
                         
   printf(" Number of BODIES (nbodies): %d \n", nbodies);
   
+  // Loop through BODIES
   for (i = 0; i < nbodies; i++)
     {
     stat = EG_getBodyTopos(bodies[i], NULL, SHELL, &n, &objs);  // Get number of SHELLS
@@ -35,37 +36,48 @@ int main(int argc, char *argv[])
     stat = EG_getBodyTopos(bodies[i], NULL, FACE, &n, &objs);  // Get number of FACES
     printf("     Number of FACES (n): %d \n", n);
     
-    stat = EG_getBodyTopos(bodies[i], NULL, LOOP, &n, &objs);  // Get number of LOOPS
-    printf("       Number of LOOPS (n): %d \n", n);
+    stat = EG_getBodyTopos(bodies[i], NULL, LOOP, &l, &lobjs);  // Get number of LOOPS
+    printf("       Number of LOOPS (n): %d \n", l);
     
-    stat = EG_getBodyTopos(bodies[i], NULL, EDGE, &n, &objs);  // Get number of EDGES
-    printf("         Number of EDGES (n): %d \n", n);
-    
-    // Loop through EDGES
-    for (j = 0; j < n; j++)
+    // Cycle through LOOPS
+    for (ll = 0; ll < l; ll++)
       {
-      index = EG_indexBodyTopo(bodies[i], objs[j]);    // Print out EDGE IDs
-      printf("          EDGE ID: %d \n", index);
+      index = EG_indexBodyTopo(bodies[i], lobjs[ll]);    // Print out Loop IDs
+      printf("          LOOP ID: %d \n", index);
       
-      // Get NODE info which associated with the current EDGE
-      stat = EG_getTopology(objs[j], &geom, &oclass, &mtype, NULL, &nn,
-                        &nobjs, &senses);
+      // Get EDGE info which associated with the current LOOP
+      stat = EG_getTopology(lobjs[ll], &geom, &oclass, &mtype, NULL, &n,
+                        &objs, &senses);
       
-      // Loop through NODES
-      for (k = 0; k < nn; k++)
+      //stat = EG_getBodyTopos(bodies[i], NULL, EDGE, &n, &objs);  // Get number of EDGES
+      //printf("         Number of EDGES (n): %d \n", n);
+      
+      // Cycle through EDGES
+      for (j = 0; j < n; j++)
         {
-        // Get Current NODE data
-        stat = EG_getTopology(nobjs[k], &geom, &oclass, &mtype, limits, &mm,
-                        &mobjs, &senses);
+        index = EG_indexBodyTopo(bodies[i], objs[j]);    // Print out EDGE IDs
+        printf("          EDGE ID: %d \n", index);
         
-        index = EG_indexBodyTopo(bodies[i], nobjs[k]);    // Print out NODE IDs & coordinates
-        printf("            NODE ID: %d \n", index);
-        printf("               (x, y, z) = ( %lf, %lf, %lf) \n", limits[0], limits[1], limits[2]);
-        } 
+        // Get NODE info which associated with the current EDGE
+        stat = EG_getTopology(objs[j], &geom, &oclass, &mtype, NULL, &nn,
+                          &nobjs, &senses);
+        
+        // Cycle through NODES
+        for (k = 0; k < nn; k++)
+          {
+          // Get Current NODE data
+          stat = EG_getTopology(nobjs[k], &geom, &oclass, &mtype, limits, &mm,
+                          &mobjs, &senses);
+          
+          index = EG_indexBodyTopo(bodies[i], nobjs[k]);    // Print out NODE IDs & coordinates
+          printf("            NODE ID: %d \n", index);
+          printf("               (x, y, z) = ( %lf, %lf, %lf) \n", limits[0], limits[1], limits[2]);
+          } 
+        }
+      
+      stat = EG_getBodyTopos(bodies[i], NULL, NODE, &n, &objs);  // Get number of NODES
+      printf("           Number of NODES (n): %d \n", n);
       }
-    
-    stat = EG_getBodyTopos(bodies[i], NULL, NODE, &n, &objs);  // Get number of NODES
-    printf("           Number of NODES (n): %d \n", n);
     }
 
   /* Close EGADSlite file */

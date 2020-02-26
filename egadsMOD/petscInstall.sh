8d77a163003f0d14b3f5efea49bd0d3478748bfd
root <root@BrandonXPS.localdomain>
2020-02-25 19:56:00 -0500

    Updated code to latest version

--- egadsMOD/petscInstall.sh
+++ egadsMOD/petscInstall.sh
@@ -4,37 +4,38 @@
 module unload petsc
 
 # Go to build directory
-cd /kif1/data/shared/software/builddir
+cd $UBCESLAB_SWENV_PREFIX/builddir
 
 # Remove current unpacked folder
-rm -rf petsc-3.12.0
+rm -rf petsc-3.12.4
 
 # Unpack tarball
-tar -xzf $UBCESLAB_SWENV_PREFIX/sourcesdir/petsc/petsc-lite-3.12.0.tar.gz
+tar -xzf $UBCESLAB_SWENV_PREFIX/sourcesdir/petsc/petsc-lite-3.12.4.tar.gz
 
-# Copy patch for hdf5
-cp /kif1/data/shared/software/patch/hdf5.patch /kif1/data/shared/software/builddir/petsc-3.12.0/hdf5.patch
-
-# Apply the patch
-cd /kif1/data/shared/software/builddir/petsc-3.12.0
-patch -p1 < hdf5.patch
+# Petsc v3.12.4 doesn't need the patch. Only Petsc v 3.12.0 does
+## Copy patch for hdf5
+#cp /kif1/data/shared/software/patch/hdf5.patch /kif1/data/shared/software/builddir/petsc-3.12.0/hdf5.patch
+#
+## Apply the patch
+#cd /kif1/data/shared/software/builddir/petsc-3.12.0
+#patch -p1 < hdf5.patch
 
 # Copy Modified plexrefine
-cp /kif1/data/shared/software/egadsMOD/plexrefine.c /kif1/data/shared/software/builddir/petsc-3.12.0/src/dm/impls/plex/plexrefine.c
+cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/plexrefine.c $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/plexrefine.c
 
 # Copy Modified petscvariables
 #cp /kif1/data/shared/software/egadsMOD/petscvariables /kif1/data/shared/software/builddir/petsc-3.12.0/lib/petsc/conf/petscvariables
 
 # Copy egads.py
-cp /kif1/data/shared/software/egadsMOD/egads.py /kif1/data/shared/software/builddir/petsc-3.12.0/config/BuildSystem/config/packages
+cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/egads.py $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/config/BuildSystem/config/packages
 
 # Copy makefile with EGADS library
-cp /kif1/data/shared/software/egadsMOD/makefile /kif1/data/shared/software/builddir/petsc-3.12.0/src/dm/impls/plex/makefile
+cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/makefile $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/makefile
 
 # Copy plexegads.c to correct location
-cp /kif1/data/shared/software/egadsMOD/plexegads.c /kif1/data/shared/software/builddir/petsc-3.12.0/src/dm/impls/plex/plexegads.c
+cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/plexegads.c $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/plexegads.c
 
 # Install the modified petsc code 
-cd /eng/home/bldenton/ubaceslab/build_scripts
+cd ~/ubaceslab/build_scripts
 ./build_petsc_egads_version.sh
 

0418157a54c995212398ba3678563b5a0e529d37
root <root@BrandonXPS.localdomain>
2020-06-08 00:33:36 -0400

    Updating plexegads.c to latest version. Also updated to hand internal points

--- egadsMOD/updatePlexegads.sh
+++ egadsMOD/updatePlexegads.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 echo "Updating plexegads.c in Petsc"
+echo ""
 # Copy plexegads.c to correct location
 cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/plexegads.c $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/plexegads.c
+#cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/plexegads_old_20200607.c $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/plexegads.c
 
 # Copy ctetgenerate.c to correct location
-#echo "Updating ctetgenerate.c in Petsc"
+echo "Updating ctetgenerate.c in Petsc"
+echo ""
 #cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/ctetgenerate.c $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/generators/ctetgen/ctetgenerate.c
 
-#echo "Updating tetgenerate.cxx in Petsc"
-#cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/tetgenerate.cxx $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/generators/tetgen/tetgenerate.cxx
+echo "Updating tetgenerate.cxx in Petsc"
+echo ""
+cp /mnt/c/Users/Brandon/Documents/School/Dissertation/Software/EGADSlite/egadsMOD/tetgenerate_EGADs.cxx $UBCESLAB_SWENV_PREFIX/builddir/petsc-3.12.4/src/dm/impls/plex/generators/tetgen/tetgenerate.cxx
 
 # Update Petsc
+echo "Updating Petsc"
 cd /mnt/c/Users/Brandon/software/builddir/petsc-3.12.4
 make -f ./gmakefile
 make install

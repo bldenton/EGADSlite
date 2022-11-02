2ffa484d5df03dad4c1dbd16c315799cc085a7b9
Brandon <bldenton@buffalo.edu>
2022-11-01 23:47:41 -0400

    Copied UVMAP_LIB.h and UVMAP_LIB_INC.h to one level higher to facilitate EGADS compilation with petsc

---
+++ EGADS/util/UVMAP_LIB_INC.h
@@ -0,0 +1,50 @@
+#ifndef __UVMAP_LIB_INC_H__
+
+#define __UVMAP_LIB_INC_H__
+
+#include "uvmap_struct.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "EG_uvmapFindUV.h"
+#include "EG_uvmapGen.h"
+#include "EG_uvmap_Read.h"
+#include "EG_uvmapStructFree.h"
+#include "EG_uvmapTest.h"
+#include "EG_uvmap_Write.h"
+#include "uvmap_add.h"
+#include "uvmap_bnd_adj.h"
+#include "uvmap_chk_area_uv.h"
+#include "uvmap_chk_edge_ratio.h"
+#include "uvmap_cpu_message.h"
+#include "uvmap_find_uv.h"
+#include "uvmap_from_egads.h"
+#include "uvmap_gen.h"
+#include "uvmap_gen_uv.h"
+#include "uvmap_ibeibe.h"
+#include "uvmap_ibfibf.h"
+#include "uvmap_ibfin.h"
+#include "uvmap_iccibe.h"
+#include "uvmap_iccin.h"
+#include "uvmap_idibe.h"
+#include "uvmap_inibe.h"
+#include "uvmap_inl_uv_bnd.h"
+#include "uvmap_malloc.h"
+#include "uvmap_mben_disc.h"
+#include "uvmap_message.h"
+#include "uvmap_norm_uv.h"
+#include "uvmap_read.h"
+#include "uvmap_solve.h"
+#include "uvmap_struct_tasks.h"
+#include "uvmap_test.h"
+#include "uvmap_to_egads.h"
+#include "uvmap_version.h"
+#include "uvmap_write.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

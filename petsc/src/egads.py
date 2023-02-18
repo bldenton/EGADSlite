8eec42ece2bc4ae4b99e2472875348bb92038c20
Brandon <bldenton@buffalo.edu>
2023-02-17 20:55:36 -0500

    Update Commit ID

--- petsc/src/egads.py
+++ petsc/src/egads.py
@@ -4,7 +4,7 @@ import os
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.gitcommit         = '405dd6c6f1bcb2c635ab6a9df0c596c7a1798b0a'
+    self.gitcommit         = '9a1064db4271f15eba44f1d87fb8d328e564d07d'
     self.download          = ['git://https://github.com/bldenton/EGADSlite.git']
     self.functions         = ['EG_open']
     self.includes          = ['egads.h']
